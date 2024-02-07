562f6bf7e5df027702c98fe3003796597ed826ea
Magali <magalii.martinez@gmail.com>
2024-02-07 12:33:24 +0100

    add

--- backend/db-twitter.sql
+++ backend/db-twitter.sql
@@ -8,9 +8,17 @@ CREATE TABLE users (
 );
 
 CREATE TABLE tweet (
-    id SERIALIZABLE PRIMARY KEY,
+    id_tweet SERIALIZABLE PRIMARY KEY,
     id_user VARCHAR(10),
     message VARCHAR(140)
     create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (id_user) REFERENCES users (id_user)
+);
+
+CREATE TABLE likes (
+    id_like SERIALIZABLE PRIMARY KEY,
+    id_user VARCHAR(10),
+    id_tweet SERIALIZABLE,
+    FOREIGN key (id_user) REFERENCES users (id_user),
+    FOREIGN KEY (id_tweet) REFERENCES tweet (id_tweet)
 );
