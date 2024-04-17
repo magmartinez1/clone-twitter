90e86af796dbd27be5521e32b5d75a9393486972
Magali <magalii.martinez@gmail.com>
2024-04-17 16:05:15 +0200

    add

--- frontend/src/views/Login/index.jsx
+++ frontend/src/views/Login/index.jsx
@@ -14,7 +14,7 @@ const Login = () => {
             const response = await login(username, password);
             console.log('Respuesta del servidor:', response);
             if (response && response.token) {
-                setToken(response.token);
+                localStorage.setItem('token', response.token);
                 console.log('Token guardado:', response.token);
             } else {
                 console.error('El servidor no devolvió un token válido.');
